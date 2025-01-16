a15fecabe202213dbedaa7b58f68c6ac87378b77
akhil <akhildongre10@gmail.com>
2025-01-16 11:46:00 +0530

    my java pattern program

---
+++ pattern.sh
@@ -0,0 +1,23 @@
+########################
+# Auhor: Akhil Dongre
+# Date: 16-01-2025
+#######################
+
+class Test
+{
+	public static void main (String [] args)
+        {
+	for (int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=i; j++)
+				{
+					System.out.println(j);
+                                                           
+				}
+
+			              System.out.print(" ");
+
+               }    
+        }
+
+}
